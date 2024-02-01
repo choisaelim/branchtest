167968a03f5bd35dc20435c63d3bd947885e53e6
choisaelim <alger426@cj.net>
2024-02-01 16:06:49 +0900

    [JIRAXXX] 기본 요구사항 반영

---
+++ OCR001_기본.py
@@ -0,0 +1,18 @@
+field_value = inputOcr
+
+'''
+import json
+
+
+# 예시 딕셔너리
+field_value = inputOcr_list
+
+
+# 딕셔너리를 JSON 형식으로 변경
+json_data = json.dumps(field_value )
+
+# JSON 값에서 필요한 값만 추출
+parsed_data = list(json.loads(json_data).values())[2]
+
+print(parsed_data)
+'''
